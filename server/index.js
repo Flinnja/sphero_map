baf84eb6b1344e222ba65fdb79d1c8c3bba84c99
Flinn Gendall <email": null>
2015-12-01 15:41:29 +1300

    add styling and fix paths for public

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ var http = require('http').Server(app)
 var path = require('path')
 
 app.use(express.static('public'))
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, '../public/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req,res){
-  res.render('index')
+  res.render('index', {title: 'SpheroMap'})
 })
 
 http.listen(3000, function(){
