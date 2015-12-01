965cc49f1c6fd9904d8e09eae8ee6f5f575c608c
Flinn Gendall <email": null>
2015-12-01 16:42:48 +1300

    create canvas element which draws dots when clicked on

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 var http = require('http').Server(app)
 var path = require('path')
+var io = require('socket.io')(http)
 
 app.use(express.static('public'))
 app.set('views', path.join(__dirname, '../public/views'));
